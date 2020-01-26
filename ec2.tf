7611f3b8e59d6d4bf13916dd56bdb44cc6de4bd1
vovatran1993 <vovatran1993@gmail.com>
2020-01-26 17:12:48 -0600

    data added

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_instance" "test" {
   ami           = "${lookup(var.ami_type,var.region)}"
   instance_type = "${lookup(var.instance_type,var.env)}"
   security_groups = "${var.sgs}"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
   
 
 
