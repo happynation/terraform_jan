9a87b497f2c54cb5ab3278b00c8373d30ffc93ba
vovatran1993 <vovatran1993@gmail.com>
2020-01-26 15:18:06 -0600

    " added

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "amitype" {}
 variable "instance_type" {
     type = "map"
     default = {
-        dev = {t2.micro}
-        qa = {t2.medium}
+        dev = "{t2.micro}"
+        qa = "{t2.medium}"
     }
 }
