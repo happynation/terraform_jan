32509bd004007eda499dbad458e2322b52513ffa
vovatran1993 <vovatran1993@gmail.com>
2020-01-27 00:11:20 -0600

    vpc_id updated

--- project1/sg_rds.tf
+++ project1/sg_rds.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "rds-prod"{
   
   name = "${var.PROJECT_NAME}-rds-production"
-  vpc_id      = "vpc-705e1217"
+  vpc_id      = "vpc-0ab1290480d3ac700"
   ingress
   {
     from_port = 3306
